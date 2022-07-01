3e7d669c1ea9b01030d461ce40a165f2870f081f
Yogesh <yogeshbietjhs@gmail.com>
2022-07-01 21:27:58 +0530

    inicial commit

---
+++ Frequency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class Frequency
+    {
+        Hashtable MyMapNode = new Hashtable();
+        
+
+        //public string[] splitString(string sentence)
+        //{
+        //    return sentence.Split();
+        //}
+
+        public void countWords(string[] sentenceSplit)
+        {
+            foreach (string key in sentenceSplit)
+            {
+                if (MyMapNode.ContainsKey(key)){
+                    MyMapNode[key]=(int)MyMapNode[key] +1;
+                }
+                else
+                {
+                    MyMapNode.Add(key,1);
+
+                }
+            }
+            foreach (DictionaryEntry item in MyMapNode)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }   
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            Frequency frequency = new Frequency();
+            string str = "To be or not to be";
+            string[] sentenceSplit = frequency.splitString(str);
+            frequency.countWords(sentenceSplit);
+
+        }
+    }
+}
